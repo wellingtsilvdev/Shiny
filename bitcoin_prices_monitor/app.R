7d24829c10da114175f18309c261d78f9ae2b979
WellingtonSilv <48716017+WellingtonSilv@users.noreply.github.com>
2020-02-04 18:08:45 -0300

    Add files via upload

---
+++ bitcoin_prices_monitor/app.R
@@ -0,0 +1,12 @@
+###########################################################
+# Wellington Silva                                        #
+# https://www.kaggle.com/gatunnopvp                       #
+# https://github.com/WellingtonSilv                       #
+# https://www.linkedin.com/in/wellington-silva-b430a1195/ #
+###########################################################
+
+# loading required libraries
+library(shiny)
+
+# creating the web app
+shinyApp(ui = ui, server = server)
